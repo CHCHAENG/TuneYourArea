0f9470a4e127ff4853b0c07f3b3896ad33bf955e
KimGaEun <dkrk333@naver.com>
2022-11-28 01:34:59 +0900

    사진 업로드(서버 연결 X)

--- app/src/main/java/com/example/tuneyourarea/SelectPage.java
+++ app/src/main/java/com/example/tuneyourarea/SelectPage.java
@@ -3,6 +3,7 @@ package com.example.tuneyourarea;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 public class SelectPage extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class SelectPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selectpage);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+      //  ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         tViewmodel = new TopViewModel(getApplicationContext());
         bViewmodel = new BottomViewModel(getApplicationContext());
 
