95b5ccb27df9d1d05602fa0843062d01334dd0b2
Chaeyoung <oco6029@naver.com>
2022-11-28 14:01:32 +0900

    develop UI

---
+++ app/src/main/java/com/example/tuneyourarea/MusicPlaylist.java
@@ -0,0 +1,17 @@
+package com.example.tuneyourarea;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MusicPlaylist extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playlistpage);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
